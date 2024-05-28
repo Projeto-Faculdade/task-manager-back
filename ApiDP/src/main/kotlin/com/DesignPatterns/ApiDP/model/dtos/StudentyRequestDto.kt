02f08b2fef89a9c955f1dd8c27e6723233bb7380
felipe <felipejiti@hotmail.com>
2024-05-28 09:51:26 -0300

    Criando Entidade Studenty e sua request que vai ser passada para comunicação

---
+++ ApiDP/src/main/kotlin/com/DesignPatterns/ApiDP/model/dtos/StudentyRequestDto.kt
@@ -0,0 +1,13 @@
+package com.DesignPatterns.ApiDP.model.dtos
+
+
+
+data class StudentyRequestDto (
+
+        var name: String,
+
+        var email: String,
+
+        var preferredLanguage: String
+
+)
