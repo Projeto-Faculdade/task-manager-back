e2f46a725aa392b9c7e14375b1c9ae7c4ff509a6
jersonb <jersonlbrito@hotmail.com>
2024-05-31 20:43:58 -0300

    feat: Update

---
+++ src/TaskManager.Application/Students/Update/StudentUpdateHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace TaskManager.Application.Students.Update;
+
+public class StudentUpdateHandler : IRequestHandler<StudentUpdateRequest>
+{
+    public Task Handle(StudentUpdateRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
