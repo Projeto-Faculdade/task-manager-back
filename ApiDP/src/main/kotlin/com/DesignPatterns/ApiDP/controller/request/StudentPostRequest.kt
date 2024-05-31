58868dd9e9e8f678b9046faae32ac4b4aec1ce1b
jersonb <jersonlbrito@hotmail.com>
2024-05-31 19:03:18 -0300

    remove kotlin

--- ApiDP/src/main/kotlin/com/DesignPatterns/ApiDP/controller/request/StudentPostRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.DesignPatterns.ApiDP.controller.request
-
-
-data class StudentPostRequest(
-
-    var name: String = "",
-    var email: String = "",
-    var preferredLanguage: String = ""
-)
