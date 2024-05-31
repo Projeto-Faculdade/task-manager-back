76d277e834aa84e5518a56fadda59cf604451df4
jersonb <jersonlbrito@hotmail.com>
2024-05-31 19:22:33 -0300

    feat: create project

---
+++ src/TaskManager.Api/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Api.Models;
+
+namespace TaskManager.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class StudentsController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Post(StudentPostRequest request)
+    {
+        return Created("api/v1/students/{id}", new { request.Id });
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetSingle(Guid id)
+    {
+        var student = new StudentPostRequest
+        {
+            Id = id
+        };
+        return Ok(student);
+    }
+}
