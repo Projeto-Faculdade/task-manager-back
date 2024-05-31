50afb680dbcbc205d72e1ccbf16886a4ee4ed031
jersonb <jersonlbrito@hotmail.com>
2024-05-31 20:35:59 -0300

    fix: swagger

--- src/TaskManager.Api/Models/StudentPostRequest.cs
+++ src/TaskManager.Api/Models/StudentPostRequest.cs
@@ -1,9 +1,15 @@
-﻿namespace TaskManager.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Api.Models;
 
 public class StudentPostRequest
 {
-    public Guid Id { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty!;
 
+    [Required]
     public string Email { get; set; } = string.Empty!;
+
+    [Required]
+    public string PreferredLanguage { get; set; } = string.Empty!;
 }
--- src/TaskManager.Api/Models/StudentPutRequest.cs
+++ src/TaskManager.Api/Models/StudentPutRequest.cs
@@ -1,11 +1,15 @@
+using System.Text.Json.Serialization;
 
 namespace TaskManager.Api.Models;
+
 public class StudentPutRequest
 {
+    [JsonIgnore]
     public Guid Id { get; set; }
 
-    public string Name { get; set; } = string.Empty!;
+    public string? Name { get; set; } = string.Empty!;
 
-    public string Email { get; set; } = string.Empty!;
+    public string? Email { get; set; } = string.Empty!;
 
+    public string? PreferredLanguage { get; set; } = string.Empty!;
 }
