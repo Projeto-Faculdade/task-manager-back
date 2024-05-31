bc272f20263f1a6466f23676767f4918058e17a1
felipe <felipejiti@hotmail.com>
2024-05-31 10:06:07 -0300

    Refatoração da camada de Controller

--- ApiDP/src/main/kotlin/com/DesignPatterns/ApiDP/controller/request/PutStudentyRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.DesignPatterns.ApiDP.controller.request
-
-data class PutStudentyRequest (
-        var name: String?,
-        var email: String?
-
-)
--- ApiDP/src/main/kotlin/com/DesignPatterns/ApiDP/model/Student.kt
+++ ApiDP/src/main/kotlin/com/DesignPatterns/ApiDP/model/Student.kt
@@ -5,12 +5,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Table
 import java.util.UUID
 
 
 @Entity
-data class Studenty (
+data class Student (
         @Id
         @GeneratedValue(strategy = GenerationType.UUID)
         var id: UUID,
