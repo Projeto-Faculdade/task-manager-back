a8cb7de9719fd411194f611cf27c701f8eaa1c71
jersonb <jersonlbrito@hotmail.com>
2024-05-31 21:52:18 -0300

    feat: cast by imploicit operator

--- src/TaskManager.Application/Students/Update/StudentUpdateRequest.cs
+++ src/TaskManager.Application/Students/Update/StudentUpdateRequest.cs
@@ -4,4 +4,11 @@ namespace TaskManager.Application.Students.Update;
 
 public class StudentUpdateRequest : IRequest
 {
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; } = string.Empty!;
+
+    public string? Email { get; set; } = string.Empty!;
+
+    public string? PreferredLanguage { get; set; } = string.Empty!;
 }
