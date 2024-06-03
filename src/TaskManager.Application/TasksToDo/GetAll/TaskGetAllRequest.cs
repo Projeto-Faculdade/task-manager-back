d22cbc4c5098e54b95b7118e3da642af1c33c93d
felipe <felipejiti@hotmail.com>
2024-06-02 22:35:58 -0300

    Adicionado o Get, GetAll, Delete

---
+++ src/TaskManager.Application/TasksToDo/GetAll/TaskGetAllRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using TaskManager.Application.TasksToDo.GetAll;
+
+namespace TaskManager.Application;
+
+public class TaskGetAllRequest : IRequest<TaskGetAllResponse>
+{
+    public string PreferredLanguage { get; set; } = string.Empty;
+}
+
+
